c99915051315c41d86c1f32867ac78f22251e83b
Hikma1 <hikma0227@gmail.com>
2026-05-03 10:58:10 +0300

    qwedsa

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ window.addEventListener("scroll", () => {
      reveals.forEach(section => {
     const top = section.getBoundingClientRect().top;
     const windowHeight = window.innerHeight;
+     if (top < windowHeight - 100) {
+      section.classList.add("active");
+    }
